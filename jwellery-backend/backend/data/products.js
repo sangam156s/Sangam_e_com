bc123f1698f9d2fda3a1dbbde4eabce33d3ec8b6
SANGAM PANDEY <sangam.pandey.ext@siemens.com>
2022-09-28 00:03:07 +0530

    Added backend

---
+++ jwellery-backend/backend/data/products.js
@@ -0,0 +1,58 @@
+const products = [
+  {
+    name: "Solitaire Diamond Rings",
+    imageUrl:
+      "https://media.istockphoto.com/photos/solitaire-diamond-engagement-ring-isolated-on-white-background-picture-id1127158685?k=20&m=1127158685&s=170667a&w=0&h=WyMayzRVPhsvB_uYNEU7Bp_eBek97-ViBUdLADpY3C0=",
+    description:
+      "Round shaped Baguette diamond rings",
+    price: 249999,
+    countInStock: 15,
+  },
+  {
+    name: "Elephant Gold Bracelet",
+    imageUrl:
+      "https://www.candere.com/media/jewellery/images/kd00018_1_2.jpg",
+    description:
+      "Golden Bracelets having elephant embroidery",
+    price: 39457,
+    countInStock: 10,
+  },
+  {
+    name: "Emerald Neck Wear",
+    imageUrl:
+      "https://staticimg.titan.co.in/Tanishq/Catalog/511183NQIAA40_1.jpg?impolicy=pqmed&imwidth=640",
+    description:
+      "Golden neck wear with emerald crafting",
+    price: 24599,
+    countInStock: 5,
+  },
+  {
+    name: "Broad Silver Anklet",
+    imageUrl:
+      "https://cdn.shopify.com/s/files/1/0577/1136/2227/products/517837_620x.jpg?v=1635249559",
+    description:
+      "Silver Anklet with broad work",
+    price: 11999,
+    countInStock: 25,
+  },
+  {
+    name: "Gold Nose Ring",
+    imageUrl:
+      "https://cf-cdn.pcjeweller.com/public/uploads/catalog/product/custom/z/ZNP00016-1.jpg",
+    description:
+      "Outfitted nose ring specially for brides",
+    price: 5999,
+    countInStock: 4,
+  },
+  {
+    name: "Gold Chain",
+    imageUrl:
+      "https://media.istockphoto.com/photos/golden-chain-with-diamond-dollar-symbol-picture-id516134632?b=1&k=20&m=516134632&s=170667a&w=0&h=xd_jenHptisF5p_ZptDGkwHoOSNczbk0MJUFLB_sfiE=",
+    description:
+      "Golden broad chain with $ symbol embedded",
+    price: 35789,
+    countInStock: 10,
+  },
+];
+
+module.exports = products;
